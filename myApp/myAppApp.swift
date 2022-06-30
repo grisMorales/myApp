8fa4788598dfacd64a2fe0bb6dfe8c55b6399880
gris <griselda.juarez@frogdesign.com>
2022-06-29 22:09:41 -0500

    Initial Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Griselda Juarez Morales on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
